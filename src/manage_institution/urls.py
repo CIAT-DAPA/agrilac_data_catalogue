626c1b7a0832e1d8d23f2b08ba5b6b9271ebaff5
Daniel Esteban Guzmán <estebantoon@hotmail.com>
2024-08-28 16:25:22 -0500

    CRUD institution added
    Working on css select for owner. Is not visualizing correctly

--- src/manage_institution/urls.py
+++ src/manage_institution/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import InstitutionListView, InstitutionCreateView, InstitutionUpdateView
+from .views import InstitutionListView, InstitutionCreateView, InstitutionUpdateView, InstitutionDeleteView
 
 urlpatterns = [
     path('', InstitutionListView.as_view(), name='institution_list'),
     path('create/', InstitutionCreateView.as_view(), name='institution_create'),
     path('update/<int:pk>/', InstitutionUpdateView.as_view(), name='institution_update'),
+    path('institutions/<int:pk>/delete/', InstitutionDeleteView.as_view(), name='institution_delete'),
+
 ]
--- src/manage_institution/views.py
+++ src/manage_institution/views.py
@@ -1,19 +1,41 @@
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Institution
 from .forms import InstitutionCreationForm, InstitutionChangeForm
 from django.contrib import messages
+from usermanagement.models import CustomUser as User
 
 
 class InstitutionListView(ListView):
     model = Institution
     template_name = 'manage_institution/institution_list.html'
+    
+    def get_queryset(self):
+        return Institution.objects.all()
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['institutions'] = self.get_queryset()
+        return context
 
 class InstitutionCreateView(CreateView):
     model = Institution
     form_class = InstitutionCreationForm
     template_name = 'manage_institution/institution_form.html'
     success_url = reverse_lazy('institution_list')
+
+    def form_valid(self, form):
+        print("Formulario es válido, datos: ", form.cleaned_data)
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        print("Formulario es inválido. Errores:", form.errors)
+        return super().form_invalid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['owners'] = User.objects.all()  # Agregar lista de usuarios al contexto
+        return context
 
 class InstitutionUpdateView(UpdateView):
     model = Institution
@@ -24,3 +46,13 @@ class InstitutionUpdateView(UpdateView):
     def form_valid(self, form):
         messages.success(self.request, "Los cambios se han guardado exitosamente.")
         return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['owners'] = User.objects.all()  # Asegúrate de que 'User' es el modelo correcto
+        return context
+
+class InstitutionDeleteView(DeleteView):
+    model = Institution
+    template_name = 'manage_institution/institution_confirm_delete.html'
+    success_url = reverse_lazy('institution_list')
