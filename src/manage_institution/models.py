b8f0f3da47e8a0123dd47054b1af013f8f1e8ee5
Daniel Esteban Guzmán <estebantoon@hotmail.com>
2024-08-30 16:23:45 -0500

    add email and phone to institution model

--- src/manage_institution/models.py
+++ src/manage_institution/models.py
@@ -8,6 +8,8 @@ class Institution(models.Model):
     is_certified = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    email = models.EmailField(blank=False)
+    phone = models.IntegerField(blank=False)
 
     def __str__(self):
         return self.name
